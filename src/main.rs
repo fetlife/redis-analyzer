0356400fd1290dbdfa76f5efba8b4e6d81c2c0b7
Rene Klacan <rene.klacan@gmail.com>
2019-07-12 12:24:14 +0200

    Gather and print memory stats

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use frequency::Frequency;
 use frequency_hashmap::HashMapFrequency;
+use pretty_bytes::converter::convert;
 use redis;
 use regex::Regex;
 use std::collections::HashMap;
@@ -8,6 +9,7 @@ pub struct PrefixStats {
     pub value: Option<String>,
     pub depth: usize,
     pub count: usize,
+    pub memory_usage: usize,
     pub subkeys: HashMap<String, PrefixStats>,
 }
 
@@ -17,6 +19,7 @@ impl PrefixStats {
             value: prefix.map(|s| s.to_string()),
             depth,
             count,
+            memory_usage: 0,
             subkeys: HashMap::new(),
         }
     }
@@ -36,7 +39,9 @@ fn main() {
 
     println!("");
 
-    print_stats(&top_stats, dbsize);
+    gather_memory_usage_stats(&mut top_stats, &mut con);
+
+    print_stats(&top_stats, top_stats.memory_usage);
 }
 
 pub fn gather_stats(prefix_stats: &mut PrefixStats, dbsize: usize, redis: &mut redis::Connection) {
@@ -50,7 +55,7 @@ pub fn gather_stats(prefix_stats: &mut PrefixStats, dbsize: usize, redis: &mut r
     let mut scan_command = redis::cmd("SCAN")
         .cursor_arg(0)
         .arg("COUNT")
-        .arg("1000")
+        .arg("100")
         .clone();
 
     if let Some(p) = &prefix_stats.value {
@@ -90,13 +95,82 @@ pub fn gather_stats(prefix_stats: &mut PrefixStats, dbsize: usize, redis: &mut r
     }
 }
 
-pub fn print_stats(prefix_stats: &PrefixStats, parent_count: usize) {
+pub fn gather_memory_usage_stats(prefix_stats: &mut PrefixStats, redis: &mut redis::Connection) {
+    let mut cursor: u64 = 0;
+    let mut total = 0;
+    let mut iterations = 0;
+    let scan_size = 100;
+    let scan_size_arg = format!("{}", scan_size);
+
+    loop {
+        if iterations % 10_000 == 0 {
+            println!("memory scan: {}", iterations);
+        }
+
+        let scan_command = redis::cmd("SCAN")
+            .cursor_arg(cursor)
+            .arg("COUNT")
+            .arg(&scan_size_arg)
+            .clone();
+
+        let (new_cursor, keys): (u64, Vec<String>) = scan_command.query(redis).expect("scan");
+
+        cursor = new_cursor;
+
+        let mut memory_usage_command = redis::pipe().clone();
+
+        for key in keys.iter() {
+            memory_usage_command = memory_usage_command
+                .cmd("MEMORY")
+                .arg("USAGE")
+                .arg(key)
+                .clone();
+        }
+
+        let memory_usages: Vec<usize> = memory_usage_command
+            .query(redis)
+            .expect("memory usage command");
+
+        for (key, memory_usage) in keys.iter().zip(memory_usages.iter()) {
+            prefix_stats.memory_usage += memory_usage;
+
+            record_memory_usage(prefix_stats, key, *memory_usage);
+        }
+
+        total += memory_usages.iter().fold(0, |a, b| a + b);
+
+        iterations += scan_size;
+
+        if cursor == 0 {
+            break;
+        }
+    }
+
+    dbg!(total);
+}
+
+pub fn record_memory_usage(prefix_stats: &mut PrefixStats, key: &str, memory_usage: usize) {
+    if prefix_stats
+        .value
+        .as_ref()
+        .map_or(true, |prefix| key.starts_with(prefix))
+    {
+        prefix_stats.memory_usage += memory_usage;
+
+        for (_, sub_prefix_stats) in prefix_stats.subkeys.iter_mut() {
+            record_memory_usage(sub_prefix_stats, key, memory_usage);
+        }
+    }
+}
+
+pub fn print_stats(prefix_stats: &PrefixStats, parent_memory_usage: usize) {
     println!(
-        "{:indent$}{} => {} ({:.2}%)",
+        "{:indent$}{} => count: {}, size: {} ({:.2}%)",
         "",
         prefix_stats.value.as_ref().unwrap_or(&"root".to_string()),
         prefix_stats.count,
-        prefix_stats.count as f32 / parent_count as f32 * 100.,
+        convert(prefix_stats.memory_usage as f64),
+        prefix_stats.memory_usage as f32 / parent_memory_usage as f32 * 100.,
         indent = prefix_stats.depth * 2,
     );
 
@@ -106,27 +180,30 @@ pub fn print_stats(prefix_stats: &PrefixStats, parent_count: usize) {
         return;
     }
 
-    subkeys.sort_by_key(|k| k.count);
+    subkeys.sort_by_key(|k| k.memory_usage);
     subkeys.reverse();
 
     let mut other = prefix_stats.count;
+    let mut other_memory_usage = prefix_stats.memory_usage;
 
     for stats in subkeys.iter() {
         other -= stats.count;
-        print_stats(stats, prefix_stats.count);
+        other_memory_usage -= stats.memory_usage;
+        print_stats(stats, prefix_stats.memory_usage);
     }
 
-    let other_percentage = other as f32 / prefix_stats.count as f32 * 100.;
+    let other_percentage = other_memory_usage as f32 / prefix_stats.memory_usage as f32 * 100.;
 
     if other_percentage < 1. {
         return;
     }
 
     println!(
-        "{:indent$}{} => {} ({:.2}%)",
+        "{:indent$}{} => count: {}, size: {} ({:.2}%)",
         "",
         "other",
         other,
+        convert(other_memory_usage as f64),
         other_percentage,
         indent = (prefix_stats.depth + 1) * 2,
     );
