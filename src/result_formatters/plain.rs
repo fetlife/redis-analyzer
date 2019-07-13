3fecf0ca6696a2b923b7baf82dd9e42e0e48510f
Rene Klacan <rene.klacan@gmail.com>
2019-07-14 00:12:22 +0200

    Clean up unused code

--- src/result_formatters/plain.rs
+++ src/result_formatters/plain.rs
@@ -25,50 +25,6 @@ pub fn call(config: &Config, root_prefix: &Prefix) {
     );
 }
 
-#[allow(dead_code)]
-fn print_stats(config: &Config, prefix: &Prefix, parent_prefix: &Prefix) {
-    println!(
-        "{:indent$}{} => count: {} ({:.2}%), size: {} ({:.2}%)",
-        "",
-        prefix.value.as_ref().unwrap_or(&"root".to_string()),
-        prefix.keys_count,
-        prefix.keys_count as f32 / parent_prefix.keys_count as f32 * 100.,
-        HumanBytes(prefix.memory_usage as u64),
-        prefix.memory_usage as f32 / parent_prefix.memory_usage as f32 * 100.,
-        indent = prefix.depth * 2,
-    );
-
-    if prefix.children.is_empty() {
-        return;
-    }
-
-    let mut other_keys_count = prefix.keys_count;
-    let mut other_memory_usage = prefix.memory_usage;
-
-    for child_prefix in prefix.children.iter() {
-        other_keys_count -= child_prefix.keys_count;
-        other_memory_usage -= child_prefix.memory_usage;
-        print_stats(config, child_prefix, prefix);
-    }
-
-    let other_keys_count_percentage = other_keys_count as f32 / prefix.keys_count as f32 * 100.;
-
-    if other_keys_count_percentage < config.min_prefix_frequency {
-        return;
-    }
-
-    println!(
-        "{:indent$}{} => count: {} ({:.2}%), size: {} ({:.2}%)",
-        "",
-        "other",
-        other_keys_count,
-        other_keys_count_percentage,
-        HumanBytes(other_memory_usage as u64),
-        other_memory_usage as f32 / prefix.memory_usage as f32 * 100.,
-        indent = (prefix.depth + 1) * 2,
-    );
-}
-
 fn print_tree(
     config: &Config,
     node: &Prefix,
@@ -104,13 +60,6 @@ fn print_tree(
             width = 30,
             info = info_string(node, parent_node, &leaf_prefix)
         );
-        // println!(
-        //     "{}{}{} {}",
-        //     prefix,
-        //     prefix_current,
-        //     key_suffix(node.value.as_ref().unwrap_or(&"".to_string()), config),
-        //     info_string(config, node, parent_node),
-        // );
     }
 
     let prefix_child = if root {
