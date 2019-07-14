0a0dcceb533994f9c68e169beb294ddd744537c0
Rene Klacan <rene.klacan@gmail.com>
2019-07-14 10:51:32 +0200

    Refactor config module

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-use clap::App;
+use clap::{App, ArgMatches};
 use regex::Regex;
 use std::process;
 
@@ -20,16 +20,10 @@ impl Config {
         let yaml = load_yaml!("cli.yml");
         let arg_matches = App::from_yaml(yaml).version(crate_version!()).get_matches();
 
-        let urls: Vec<&str> = arg_matches.value_of("urls").unwrap().split(",").collect();
         let separators = arg_matches.value_of("separators").unwrap_or(":/|");
         let max_depth = arg_matches
             .value_of("max_depth")
             .map_or(999, |s| s.parse().expect("max-depth needs to be a number"));
-        let max_parallelism = arg_matches
-            .value_of("max_parallelism")
-            .map_or(num_cpus::get(), |s| {
-                s.parse().expect("max-parallelism needs to be a number")
-            });
         let min_prefix_frequency = arg_matches
             .value_of("min_prefix_frequency")
             .map_or(1., |s| {
@@ -37,52 +31,20 @@ impl Config {
                     .expect("min-prefix-frequency needs to be a number")
             });
 
-        rayon::ThreadPoolBuilder::new()
-            .num_threads(max_parallelism)
-            .build_global()
-            .unwrap();
-
-        let databases: Vec<Database> = urls
-            .iter()
-            .map(|host| {
-                let url = format!("redis://{}", host);
-                let client = redis::Client::open(url.as_ref()).expect("connect to redis");
-                let connection = client.get_connection().expect("getting connection");
-                let keys_count: usize = redis::cmd("DBSIZE")
-                    .query(&connection)
-                    .expect("getting dbsize");
-
-                Database {
-                    host: host.to_string(),
-                    url,
-                    keys_count,
-                    connection,
-                }
-            })
-            .collect();
-
+        let databases = parse_and_build_databases(&arg_matches);
         let all_keys_count: usize = databases
             .iter()
             .fold(0, |acc, database| acc + database.keys_count);
-
-        let output_format = match arg_matches.value_of("format").unwrap_or("plain") {
-            "plain" => OutputFormat::Plain,
-            "json" => OutputFormat::Json,
-            format => {
-                eprintln!("Invalid format: {}", format);
-                process::exit(1);
-            }
-        };
 
         Self {
             databases,
             all_keys_count,
             separators: separators.to_string(),
             max_depth,
-            max_parallelism,
+            max_parallelism: parse_and_configure_max_parallelism(&arg_matches),
             min_prefix_frequency,
             progress: arg_matches.is_present("progress"),
-            output_format,
+            output_format: parse_output_format(&arg_matches),
         }
     }
     pub fn separators_regex(&self) -> Regex {
@@ -94,4 +56,57 @@ impl Config {
 pub enum OutputFormat {
     Plain,
     Json,
+}
+
+pub enum SortOrder {
+    KeysCount,
+    MemoryUsage,
+}
+
+fn parse_output_format(arg_matches: &ArgMatches) -> OutputFormat {
+    match arg_matches.value_of("format").unwrap_or("plain") {
+        "plain" => OutputFormat::Plain,
+        "json" => OutputFormat::Json,
+        format => {
+            eprintln!("Invalid format: {}", format);
+            process::exit(1);
+        }
+    }
+}
+
+fn parse_and_configure_max_parallelism(arg_matches: &ArgMatches) -> usize {
+    let max_parallelism = arg_matches
+        .value_of("max_parallelism")
+        .map_or(num_cpus::get(), |s| {
+            s.parse().expect("max-parallelism needs to be a number")
+        });
+
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(max_parallelism)
+        .build_global()
+        .unwrap();
+
+    max_parallelism
+}
+
+fn parse_and_build_databases(arg_matches: &ArgMatches) -> Vec<Database> {
+    let urls: Vec<&str> = arg_matches.value_of("urls").unwrap().split(",").collect();
+
+    urls.iter()
+        .map(|host| {
+            let url = format!("redis://{}", host);
+            let client = redis::Client::open(url.as_ref()).expect("connect to redis");
+            let connection = client.get_connection().expect("getting connection");
+            let keys_count: usize = redis::cmd("DBSIZE")
+                .query(&connection)
+                .expect("getting dbsize");
+
+            Database {
+                host: host.to_string(),
+                url,
+                keys_count,
+                connection,
+            }
+        })
+        .collect()
 }
