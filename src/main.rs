f9992384240143e2483e17b95e60f61c4676a90e
Andrii Dmytrenko <refresh.xss@gmail.com>
2025-11-03 11:16:19 +0000

    Enable rustls

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use color_eyre::Result;
+use color_eyre::eyre::eyre;
 
 use crate::config::Config;
 
@@ -9,6 +10,9 @@ pub(crate) mod key_prefix;
 pub(crate) mod result_formatters;
 
 fn main() -> Result<()> {
+    rustls::crypto::ring::default_provider()
+        .install_default()
+        .map_err(|e| eyre!("failed to install default crypto provider: {:?}", e))?;
     let mut config = Config::new()?;
     let result = analyzer::run(&mut config);
 
